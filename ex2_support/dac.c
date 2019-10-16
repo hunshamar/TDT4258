2b33e1f8123d2e546a57a56f33618ab122dbd5e0
Marcus Bjørnå <marcusbjorna@gmail.com>
2019-10-16 13:27:55 +0200

    mulig fiks i timer1irqhandler

--- ex2_support/dac.c
+++ ex2_support/dac.c
@@ -23,7 +23,7 @@ void setupDAC()
 	*DAC0_CTRL = 0x50010; // Prescale DAC clock by writing 0x50010 to DAC0_CTRL
 	*DAC0_CH0CTRL = 1; // Enable left audo channel
 	*DAC0_CH1CTRL = 1; // Enable right audio channel
-	
+
 
 
 
