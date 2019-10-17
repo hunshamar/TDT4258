898e4d888d4191f7665e1120f6ba084c5e0ff624
Asgeir Hunshamar <asgeirhuns@gmail.com>
2019-10-17 14:44:37 +0200

    easier to read code for DAC

--- ex2_support/dac.c
+++ ex2_support/dac.c
@@ -1,14 +1,19 @@
 #include "dac.h"
 
-#define DAC_CLOCK_BIT 17
+#define DAC_CLOCK_BIT 	17
+#define PRESC 			5
+#define MODE_PIN 		1
+
 
 void setupDAC()
 {
 	*CMU_HFPERCLKEN0 |= (1 << DAC_CLOCK_BIT); // Enable the DAC by setting bit 17 in CMU_HFPERCLKEN0
-	*DAC0_CTRL = 0x50010; // Prescale DAC clock by writing 0x50010 to DAC0_CTRL
+	*DAC0_CTRL |= (PRESC << 16);  // Prescale DAC clock by writing 5 to pin 16-18
+	*DAC0_CTRL |= (MODE_PIN << 4); // set PIN output mode by writing 1 to pin 4
 	*DAC0_CH0CTRL = 1; // Enable left audo channel
 	*DAC0_CH1CTRL = 1; // Enable right audio channel
 }
+
 
 
 
