c0b439df2ce8a3d48d99bcec6eb9ffdc35017328
Marcus Bjørnå <marcusbjorna@gmail.com>
2019-10-17 11:47:17 +0200

    bytta type på set-sound

--- ex2_support/sound.h
+++ ex2_support/sound.h
@@ -2,7 +2,7 @@
 #define SOUND_H
 
 void play_sound();
-void set_sound_type(sound_type);
+void set_sound_type(uint8_t sound);
 void change_volume(int delta_volume);
 void change_frequency(int delta_freq);
 
